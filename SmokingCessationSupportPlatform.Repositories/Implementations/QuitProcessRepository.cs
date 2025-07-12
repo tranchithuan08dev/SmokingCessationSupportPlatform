99e204381b9b43ab387609b8e0393daa27a5666b
ChiThuan <tranchithuanjr@gmail.com>
2025-07-12 23:18:31 +0700

    create service QuitProcess

---
+++ SmokingCessationSupportPlatform.Repositories/Implementations/QuitProcessRepository.cs
@@ -0,0 +1,21 @@
+﻿using SmokingCessationSupportPlatform.BusinessObjects.Models;
+using SmokingCessationSupportPlatform.DataAccessObjects;
+using SmokingCessationSupportPlatform.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmokingCessationSupportPlatform.Repositories.Implementations
+{
+    public class QuitProcessRepository : IQuitProcessRepository
+    {
+      
+        // Ghi lại quá trình bỏ thuốc lá của người dùng
+        public void CreateQuitProcess(QuitProgress quitProgress)
+        {
+           QuitProcessDAO.CreateQuitProcess(quitProgress);
+        }
+    }
+}
