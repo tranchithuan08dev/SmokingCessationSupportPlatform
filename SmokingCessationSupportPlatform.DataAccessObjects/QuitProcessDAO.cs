f7df2435cde1a0f903e6687fc4992107d2c44055
ChiThuan <tranchithuanjr@gmail.com>
2025-07-12 23:17:25 +0700

    create fuction add quitProgress

---
+++ SmokingCessationSupportPlatform.DataAccessObjects/QuitProcessDAO.cs
@@ -0,0 +1,28 @@
+﻿using SmokingCessationSupportPlatform.BusinessObjects.Models;
+using SmokingCessationSupportPlatform.DataAccessObjects.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmokingCessationSupportPlatform.DataAccessObjects
+{
+      public class QuitProcessDAO
+      {
+        // Ghi lại quá trình bỏ thuốc lá của người dùng 
+        public static void CreateQuitProcess(QuitProgress quitProgress)
+        {
+            try
+            {
+                using var context = new SmokingCessationSupportPlatformContext();
+                context.QuitProgresses.Add(quitProgress);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating quit process: {ex.Message}");
+            }
+        }
+      }
+}
